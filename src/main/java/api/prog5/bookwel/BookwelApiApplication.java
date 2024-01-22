1687a1a57f2ad8e626298f847fa2ef5c8e7076bc
Onitsiky <hei.onitsiky@gmail.com>
2024-01-22 08:49:34 +0300

    feat: initialize project

---
+++ src/main/java/api/prog5/bookwel/BookwelApiApplication.java
@@ -0,0 +1,13 @@
+package api.prog5.bookwel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwelApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookwelApiApplication.class, args);
+  }
+
+}
