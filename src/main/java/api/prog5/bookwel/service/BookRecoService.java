8d56eb146b7cebe7661669cf553aaf34d24202ff
dinasoa <hei.dinasoa@gmail.com>
2024-01-30 10:12:44 +0300

    feat: add openai deps

---
+++ src/main/java/api/prog5/bookwel/service/BookRecoService.java
@@ -0,0 +1,11 @@
+package api.prog5.bookwel.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class BookRecoService {
+
+
+}
