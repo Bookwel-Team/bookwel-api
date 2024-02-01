6e2ca636a97485c10b9f6befaa2133b490352323
Mahefaa <hei.mahefa@gmail.com>
2024-02-01 22:57:45 +0300

    chore(test): test users and books note: delete test migrations and db/note file

--- src/test/java/api/prog5/bookwel/utils/TestUtils.java
+++ src/test/java/api/prog5/bookwel/utils/TestUtils.java
@@ -1,3 +1,15 @@
 package api.prog5.bookwel.utils;
 
-public class TestUtils {}
+import api.prog5.bookwel.endpoint.rest.client.ApiClient;
+
+public class TestUtils {
+  public static ApiClient anApiClient(String token, int serverPort) {
+    ApiClient client = new ApiClient();
+    client.setScheme("http");
+    client.setHost("localhost");
+    client.setPort(serverPort);
+    client.setRequestInterceptor(
+        httpRequestBuilder -> httpRequestBuilder.header("Authorization", "Bearer " + token));
+    return client;
+  }
+}
