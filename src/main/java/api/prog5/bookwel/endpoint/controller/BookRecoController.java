df23e8995cff1586dd6cd9e0a67316710606e6da
dinasoa <hei.dinasoa@gmail.com>
2024-02-02 13:18:18 +0300

    chore: add user prompt in promptTemplate

--- src/main/java/api/prog5/bookwel/endpoint/controller/BookRecoController.java
+++ src/main/java/api/prog5/bookwel/endpoint/controller/BookRecoController.java
@@ -4,18 +4,20 @@ import api.prog5.bookwel.service.BookRecoService;
 import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.Generation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class BookeRecoController {
+public class BookRecoController {
 
   private final BookRecoService bookRecoService;
 
   @GetMapping("/book")
   public Generation bookReco(
-    @RequestParam String title, @RequestParam String author, @RequestParam String category) {
+    @RequestParam(name = "title") String title, @RequestParam(name = "author") String author, @RequestParam(name = "category") String category) {
     return bookRecoService.bookReco(title, author, category);
   }
+
 }
--- src/main/java/api/prog5/bookwel/service/BookRecoService.java
+++ src/main/java/api/prog5/bookwel/service/BookRecoService.java
@@ -1,47 +1,46 @@
 package api.prog5.bookwel.service;
 
+import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.Generation;
-import org.springframework.ai.prompt.Prompt;
-import org.springframework.ai.prompt.PromptTemplate;
-import org.springframework.ai.prompt.SystemPromptTemplate;
-import org.springframework.ai.prompt.messages.Message;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
 
 @Service
 @AllArgsConstructor
 public class BookRecoService {
-  private static ChatClient chatClient;
+  private final ChatClient chatClient;
 
   public Generation bookReco(String title, String author, String category) {
     String systemPrompt = """
-                You are an AI Literary Recommendation Engine, providing book suggestions based on the given book titles, genres, and descriptions.
-                """;
+                    You are an AI Literary Recommendation Engine, providing book suggestions based on the given book titles, genres, and descriptions.
+                    """;
+
     SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemPrompt);
     Message systemMessage = systemPromptTemplate.createMessage();
 
-    // Create a prompt template
     PromptTemplate promptTemplate = new PromptTemplate("""
-                %INSTRUCTIONS:
-                Task:
-                As an AI literary recommendation engine, your task is to analyze one or more {book titles with their descriptions} provided by the user.
-                Based on the title, genres, and synopsis of each given book, you should suggest similar lesser-known books that the user might enjoy.
-                Your recommendations should come from a diverse range of authors and prioritize books that aren't mainstream or widely popular but that are equally good or even better than the provided books.
-                Your recommendations should not be bound by publication date or a fiction/non-fiction distinction unless specified in the input.
-                For every recommended book, provide a brief synopsis (up-to 3 lines) and the reason for your choice.
-                Provide up-to 3 book suggestions.
-                When suggesting books, use below format.
-                Title:
-                Author:
-                Synopsis:
-                Reason for Recommendation:
+                    %INSTRUCTIONS:
+                    Task:
+                    As an AI literary recommendation engine, your task is to analyze this book having this tiltle:  {title}, its genre is: {category}, written by {author} , provided by the user %s.
+                    Based on the title, genres, and synopsis of each given book, you should suggest similar lesser-known books that the user might enjoy %s.
+                    Your recommendations should come from a diverse range of authors and prioritize books that aren't mainstream or widely popular but that are equally good or even better than the provided books %s.
+                    Your recommendations should not be bound by publication date or a fiction/non-fiction distinction unless specified in the input %s.
+                    For every recommended book, provide a brief synopsis (up-to 3 lines) and the reason for your choice %s.
+                    Provide up-to 3 book suggestions %s.
+                    When suggesting books, use below format %s.
+                    Title:
+                    Author:
+                    Synopsis:
+                    Category:
+                    Reason for Recommendation:
 
-                %BOOK TITLES WITH THEIR DESCRIPTIONS:
-                """);
+                    """);
 
     Message userMessage = promptTemplate.createMessage(Map.of(
         "title", title,
@@ -50,7 +49,6 @@ public class BookRecoService {
     ));
 
     Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
-
-    return chatClient.generate(prompt).getGeneration();
+    return chatClient.call(prompt).getResult();
   }
 }
