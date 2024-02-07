b55865ef145b66de5e6541c647da7fd0453b9ef6
dinasoa <hei.dinasoa@gmail.com>
2024-02-07 22:31:18 +0300

    test: configure bucket conf

---
+++ src/test/java/api/prog5/bookwel/conf/BucketConf.java
@@ -0,0 +1,10 @@
+package api.prog5.bookwel.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class BucketConf {
+
+    void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("aws.s3.bucket", () -> "dummy-bucket");
+    }
+}
