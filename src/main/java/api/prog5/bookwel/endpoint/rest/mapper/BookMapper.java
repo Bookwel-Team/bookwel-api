027e2a77ecf1fa535036b5924952740bc2f7dd00
dinasoa <hei.dinasoa@gmail.com>
2024-02-07 22:30:26 +0300

    chore: add getPresignedUrl from filename

--- src/main/java/api/prog5/bookwel/endpoint/rest/mapper/BookMapper.java
+++ src/main/java/api/prog5/bookwel/endpoint/rest/mapper/BookMapper.java
@@ -1,18 +1,22 @@
 package api.prog5.bookwel.endpoint.rest.mapper;
 
 import api.prog5.bookwel.endpoint.rest.model.Book;
+import api.prog5.bookwel.service.BookService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class BookMapper {
+  private BookService bookService;
 
   public Book toRest(api.prog5.bookwel.repository.model.Book domain) {
+    String filename = domain.getFilename();
     return new Book()
         .id(domain.getId())
         .author(domain.getAuthor())
-        .fileLink(domain.getFileLink())
+        .fileName(filename)
+        .fileLink(bookService.getPresignedUrlFromFilename(filename).toString())
         .category(domain.getCategory().getName())
         .title(domain.getTitle());
   }
--- src/main/java/api/prog5/bookwel/service/BookService.java
+++ src/main/java/api/prog5/bookwel/service/BookService.java
@@ -3,9 +3,13 @@ package api.prog5.bookwel.service;
 import static org.springframework.data.domain.Sort.Direction.DESC;
 
 import api.prog5.bookwel.endpoint.rest.exception.NotFoundException;
+import api.prog5.bookwel.file.BucketComponent;
 import api.prog5.bookwel.repository.BookRepository;
 import api.prog5.bookwel.repository.dao.BookDao;
 import api.prog5.bookwel.repository.model.Book;
+
+import java.net.URL;
+import java.time.Duration;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 public class BookService {
   private final BookDao bookDao;
   private final BookRepository bookRepository;
+  private final BucketComponent bucketComponent;
 
   public List<Book> getAllByCriteria(
       String author, String category, Integer page, Integer pageSize) {
@@ -29,5 +34,9 @@ public class BookService {
     return bookRepository
         .findById(id)
         .orElseThrow(() -> new NotFoundException("Book with id: " + id + " not found"));
+  }
+
+  public URL getPresignedUrlFromFilename(String filename){
+    return bucketComponent.presign(filename, Duration.ofMinutes(2));
   }
 }
