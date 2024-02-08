994dcb214bc64234198a3955a112c0cc201c9fdd
Mahefaa <hei.mahefa@gmail.com>
2024-02-08 19:48:46 +0300

    chore: remove unused exceptions

--- src/main/java/api/prog5/bookwel/endpoint/rest/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package api.prog5.bookwel.endpoint.rest.exception;
-
-public class BadRequestException extends ApiException {
-  public BadRequestException(String message) {
-    super(ExceptionType.CLIENT_EXCEPTION, message);
-  }
-}
