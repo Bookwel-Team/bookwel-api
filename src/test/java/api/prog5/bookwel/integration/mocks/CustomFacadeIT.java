3300138c8a0a23216472c5b5265472a506985ce8
Mahefaa <hei.mahefa@gmail.com>
2024-02-08 15:32:00 +0300

    fix: missing aws.region for BucketConf

--- src/test/java/api/prog5/bookwel/integration/mocks/CustomFacadeIT.java
+++ src/test/java/api/prog5/bookwel/integration/mocks/CustomFacadeIT.java
@@ -4,13 +4,17 @@ import static api.prog5.bookwel.integration.mocks.MockData.USER_ONE_EMAIL;
 import static api.prog5.bookwel.integration.mocks.MockData.USER_ONE_ID_TOKEN;
 import static api.prog5.bookwel.integration.mocks.MockData.USER_TWO_EMAIL;
 import static api.prog5.bookwel.integration.mocks.MockData.USER_TWO_ID_TOKEN;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import api.prog5.bookwel.conf.FacadeIT;
 import api.prog5.bookwel.endpoint.rest.security.auth.firebase.FirebaseAuthenticator;
 import api.prog5.bookwel.endpoint.rest.security.auth.firebase.FirebaseConf;
+import api.prog5.bookwel.file.BucketComponent;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -19,14 +23,19 @@ public class CustomFacadeIT extends FacadeIT {
   @MockBean protected FirebaseApp firebaseAppMock;
   @MockBean protected FirebaseAuth firebaseAuthMock;
   @MockBean protected FirebaseAuthenticator firebaseAuthenticator;
+  @MockBean protected BucketComponent bucketComponent;
 
   protected void setupFirebaseAuthenticator(FirebaseAuthenticator firebaseAuthenticator) {
     when(firebaseAuthenticator.getEmail(USER_ONE_ID_TOKEN)).thenReturn(USER_ONE_EMAIL);
     when(firebaseAuthenticator.getEmail(USER_TWO_ID_TOKEN)).thenReturn(USER_TWO_EMAIL);
   }
 
+  protected void setupBucketComponent(BucketComponent bucketComponent) throws MalformedURLException {
+    when(bucketComponent.presign(any(), any())).thenReturn(new URL("http://localhost"));
+  }
   @BeforeEach
-  void setup() {
+  void setup() throws MalformedURLException {
     setupFirebaseAuthenticator(firebaseAuthenticator);
+    setupBucketComponent(bucketComponent);
   }
 }
