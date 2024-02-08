af799d30bc5523639c18b80ff767ca784532cd19
Mahefaa <hei.mahefa@gmail.com>
2024-02-08 19:48:46 +0300

    chore: remove unused exceptions

--- src/main/java/api/prog5/bookwel/endpoint/rest/exception/NotImplementedException.java
+++
@@ -1,7 +0,0 @@
-package api.prog5.bookwel.endpoint.rest.exception;
-
-public class NotImplementedException extends ApiException {
-  public NotImplementedException(String message) {
-    super(ExceptionType.SERVER_EXCEPTION, message);
-  }
-}
