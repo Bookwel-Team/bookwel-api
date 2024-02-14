56339569ea876ebdc39a9f0a0b0db7f27ada7c07
Mahefaa <hei.mahefa@gmail.com>
2024-02-14 19:32:12 +0300

    fix: bad request are not correctly mapped

--- src/main/java/api/prog5/bookwel/endpoint/InternalToRestExceptionHandler.java
+++ src/main/java/api/prog5/bookwel/endpoint/InternalToRestExceptionHandler.java
@@ -1,25 +1,69 @@
 package api.prog5.bookwel.endpoint;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import api.prog5.bookwel.endpoint.rest.exception.BadRequestException;
 import api.prog5.bookwel.endpoint.rest.exception.ForbiddenException;
 import api.prog5.bookwel.endpoint.rest.exception.NotFoundException;
+import api.prog5.bookwel.endpoint.rest.model.Exception;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 @Slf4j
 public class InternalToRestExceptionHandler {
 
-  @ExceptionHandler(value = {Exception.class})
-  ResponseEntity<api.prog5.bookwel.endpoint.rest.model.Exception> handleDefault(Exception e) {
+  @ExceptionHandler(value = {BadRequestException.class})
+  ResponseEntity<Exception> handleBadRequest(BadRequestException e) {
+    log.info("Bad request", e);
+    return new ResponseEntity<>(toRest(e, BAD_REQUEST), BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = {DataIntegrityViolationException.class})
+  ResponseEntity<Exception> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+    log.info("Bad request", e);
+    return new ResponseEntity<>(toRest(e, BAD_REQUEST), BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = {MissingServletRequestParameterException.class})
+  ResponseEntity<Exception> handleBadRequest(MissingServletRequestParameterException e) {
+    log.info("Missing parameter", e);
+    return handleBadRequest(new BadRequestException(e.getMessage()));
+  }
+
+  @ExceptionHandler(value = {HttpRequestMethodNotSupportedException.class})
+  ResponseEntity<Exception> handleBadRequest(HttpRequestMethodNotSupportedException e) {
+    log.info("Unsupported method for this endpoint", e);
+    return handleBadRequest(new BadRequestException(e.getMessage()));
+  }
+
+  @ExceptionHandler(value = {HttpMessageNotReadableException.class})
+  ResponseEntity<Exception> handleBadRequest(HttpMessageNotReadableException e) {
+    log.info("Missing required body", e);
+    return handleBadRequest(new BadRequestException(e.getMessage()));
+  }
+
+  @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
+  ResponseEntity<Exception> handleConversionFailed(MethodArgumentTypeMismatchException e) {
+    log.info("Conversion failed", e);
+    String message = e.getCause().getCause().getMessage();
+    return handleBadRequest(new BadRequestException(message));
+  }
+  @ExceptionHandler(value = {java.lang.Exception.class})
+  ResponseEntity<api.prog5.bookwel.endpoint.rest.model.Exception> handleDefault(java.lang.Exception e) {
     log.error("Internal error", e);
     return new ResponseEntity<>(toRest(e, INTERNAL_SERVER_ERROR), INTERNAL_SERVER_ERROR);
   }
@@ -30,7 +74,7 @@ public class InternalToRestExceptionHandler {
         ForbiddenException.class,
         AuthenticationException.class
       })
-  ResponseEntity<api.prog5.bookwel.endpoint.rest.model.Exception> handleForbidden(Exception e) {
+  ResponseEntity<api.prog5.bookwel.endpoint.rest.model.Exception> handleForbidden(java.lang.Exception e) {
     /* rest.model.Exception.Type.FORBIDDEN designates both authentication and authorization errors.
      * Hence do _not_ HttpsStatus.UNAUTHORIZED because, counter-intuitively,
      * it's just for authentication.
