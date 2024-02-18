383739fbdffd3e97c0b50c9bbac6d6c3fd62f21b
Mahefaa <hei.mahefa@gmail.com>
2024-02-18 05:15:26 +0300

    fix: BookReaction Id gen is automated

--- src/main/java/api/prog5/bookwel/repository/model/BookReaction.java
+++ src/main/java/api/prog5/bookwel/repository/model/BookReaction.java
@@ -1,12 +1,14 @@
 package api.prog5.bookwel.repository.model;
 
 import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static org.hibernate.type.SqlTypes.NAMED_ENUM;
 
 import api.prog5.bookwel.endpoint.rest.model.ReactionStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -30,7 +32,9 @@ import org.hibernate.annotations.JdbcTypeCode;
 @ToString
 @Table(name = "book_reaction")
 public class BookReaction {
-  @Id private String id;
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private String id;
 
   @Enumerated(STRING)
   @Column(name = "reaction")
